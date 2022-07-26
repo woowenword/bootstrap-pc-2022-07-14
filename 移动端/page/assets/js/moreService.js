b768ea0f6f3959e41e261f8a4ef33e5802c9af7c
wuwenwen <474000418@qq.com>
2022-07-26 22:08:35 +0800

    修改更多服务

--- 移动端/page/assets/js/moreService.js
+++ 移动端/page/assets/js/moreService.js
@@ -4,13 +4,9 @@ let useFrequentlyUl = document.getElementById('use-frequently-ul');
 let dialog = document.getElementById('dialog');
 // 定义添加动作的定时器
 let timeOutEvent = 0;
-let longClick = 0;
+
 // 定义删除动作的定时器
 let timeOutEventDel = 0;
-let longClickDel = 0;
-
-
-
 
 
 // ssc常用服务入口渲染
@@ -20,7 +16,7 @@ function renderHtml() {
   useFrequentlyArr.length > 0 && useFrequentlyArr.forEach((item, index) => {
     useFrequentlyStr += `<li class="press-img">
       <img src="${item.src}" />
-      <i class="bi bi-dash-circle-fill icon-position none" markdel="${index}"></i>
+      <i class="bi bi-dash-circle-fill icon-position none" markdel="${item.num}"></i>
       <p>${item.text}</p>
     </li>`
   })
@@ -40,27 +36,16 @@ sscServeUl.addEventListener('touchstart', function (ev) {
   if (target.tagName === 'IMG') { // 点击图片则会显示添加按钮
 
     timeOutEvent && clearTimeout(timeOutEvent); //开启定时器前，先清除定时器，防止重复触发
-    longClick = 0;
 
     timeOutEvent = setTimeout(function () { //此处为长按事件-----在此显示遮罩层及按钮
 
-      longClick = 1;
-      showOrHidDialog('show'); // 显示遮照层
+      addStyle(target);
 
-      let elseLi = target.parentNode.parentNode.children;
-      for (var i = 0, elseLil = elseLi.length; i < elseLil; i++) {
-        if (elseLi[i].children[1]) {
-          elseLi[i].classList.add('shake');
-          elseLi[i].children[1].classList.remove('none');
-          elseLi[i].children[1].classList.add('block');
-        }
-      }
     }, 500)
 
     ev.preventDefault() // 阻止系统默认事件
     return false;
   } else {
-    longClick = 0;
     clearTimeout(timeOutEvent);
 
     if (target.tagName === 'I') { // 点击添加
@@ -72,22 +57,25 @@ sscServeUl.addEventListener('touchstart', function (ev) {
 
         // 如果数组中存在某一icon,则不添加
         if (useFrequentlyArr.some(item => item.num == target.attributes.markplus.value)) {
+          alert('已经添加过了哦')
           return false
         } else {
           pushArr()
         }
-      } else {
+      } else { // 如果数组为空数组，则直接push
         pushArr()
       }
-      renderHtml();
-      delStyle();
-      
-    } else if(target.tagName === 'UL'){
+      renderHtml(); // 渲染页面
+
+      delStyle(target); // 去除style样式
+
+    } else if (target.tagName === 'UL') {
       return false
     } else { // 点击其他地方
-      delStyle()
+      delStyle(target)
     }
   }
+
   function pushArr() {
     useFrequentlyArr.push({
       num: target.attributes.markplus.value,
@@ -96,24 +84,7 @@ sscServeUl.addEventListener('touchstart', function (ev) {
     })
   }
 
-  function delStyle(){
-    let elseLi;
-      if (target.className.indexOf('press-img') > -1) { //点击的是父容器
-        elseLi = target.parentNode.children;
-      } else {
-        elseLi = target.parentNode.parentNode.children;
-      }
-      showOrHidDialog('hide'); // 隐藏遮照层
-      for (var i = 0, elseLil = elseLi.length; i < elseLil; i++) {
-        if (elseLi[i].children[1]) {
-          elseLi[i].classList.remove('shake');
-          elseLi[i].children[1].classList.add('none');
-          elseLi[i].children[1].classList.remove('block');
-        }
-      }
-  }
 })
-
 
 // 单击遮照层--按钮&样式消失
 dialog.addEventListener('touchend', function (ev) {
@@ -136,29 +107,23 @@ dialog.addEventListener('touchend', function (ev) {
 useFrequentlyUl.addEventListener('touchstart', function (ev) {
   let target = ev.target;
   if (target.tagName === 'IMG') { // 点击图片则会显示添加按钮
-    //开启定时器前，先清除定时器，防止重复触发
-    timeOutEventDel && clearTimeout(timeOutEventDel);
-    longClickDel = 0; //设置初始为0
 
-    timeOutEventDel = setTimeout(function () {
-      //此处为长按事件-----在此显示遮罩层及删除按钮
-      longClickDel = 1;
-      showOrHidDialog('show'); // 显示遮照层
+    timeOutEventDel && clearTimeout(timeOutEventDel); //开启定时器前，先清除定时器，防止重复触发
 
-      let elseLi = target.parentNode.parentNode.children;
-      for (var i = 0, elseLil = elseLi.length; i < elseLil; i++) {
-        if (elseLi[i].children[1]) {
-          elseLi[i].classList.add('shake');
-          elseLi[i].children[1].classList.remove('none');
-          elseLi[i].children[1].classList.add('block');
-        }
-      }
+    timeOutEventDel = setTimeout(function () { //此处为长按事件-----在此显示遮罩层及删除按钮
+
+      addStyle(target);
+
     }, 500)
+
+    ev.preventDefault() // 阻止系统默认事件
+    return false;
   } else {
     clearTimeout(timeOutEventDel);
+
     if (target.tagName === 'I') {
       if (useFrequentlyArr.length > 1) {
-        useFrequentlyArr.forEach((item, index) => {
+        useFrequentlyArr.some((item, index) => {
           if (item.num == target.attributes.markdel.value) {
             useFrequentlyArr.splice(index, 1)
           }
@@ -169,64 +134,49 @@ useFrequentlyUl.addEventListener('touchstart', function (ev) {
       showOrHidDialog('hide');
       renderHtml()
       return false;
+    } else if (target.tagName === 'UL') {
+      return false
     } else {
-      let elseLi;
-
-      if (target.className.indexOf('js-icon-wrap') > -1) { //点击的是父容器
-        elseLi = target.parentNode.children;
-      } else {
-        elseLi = target.parentNode.parentNode.children;
-      }
-      showOrHidDialog('hide'); // 隐藏遮照层
-      for (var i = 0, elseLil = elseLi.length; i < elseLil; i++) {
-        if (elseLi[i].children[1]) {
-          elseLi[i].classList.remove('shake');
-          elseLi[i].children[1].classList.add('none');
-          elseLi[i].children[1].classList.remove('block');
-        }
-      }
+      delStyle(target);
     }
   }
-})
-
-// 删除动作： 第二步：点击删除按钮
-useFrequentlyUl.addEventListener('touchend', function (ev) {
-  let target = ev.target;
-  clearTimeout(timeOutEventDel);
-  if (target.tagName === 'I') { // 点击的是删除按钮
-    if (timeOutEventDel != 0 && longClickDel == 0) { //点击
-      //此处为点击事件----在此处添加跳转详情页
-      useFrequentlyArr.length > 0 && useFrequentlyArr.forEach((item, index) => {
-        if (item.num == target.attributes.markdel.value) {
-          useFrequentlyArr.splice(index, 1)
-          renderHtml()
-        }
-      })
-    }
-    return false;
-  } else {
-    let elseLi;
-
-    if (target.className.indexOf('js-icon-wrap') > -1) { //点击的是父容器
-      elseLi = target.children;
-    } else {
-      elseLi = target.parentNode.parentNode.children;
-    }
-    showOrHidDialog('hide'); // 隐藏遮照层
-    for (var i = 0, elseLil = elseLi.length; i < elseLil; i++) {
-      if (elseLi[i].children[1]) {
-        elseLi[i].classList.remove('shake');
-        elseLi[i].children[1].classList.add('none');
-        elseLi[i].children[1].classList.remove('block');
-      }
-    }
-  }
-
 })
 
 useFrequentlyUl.addEventListener('touchmove', function (ev) {
   clearTimeout(timeOutEventDel);
 })
+
+
+function delStyle(domEvent) {
+  showOrHidDialog('hide'); // 隐藏遮照层
+
+  let elseLi;
+  if (domEvent.className.indexOf('press-img') > -1) { //点击的是父容器
+    elseLi = domEvent.parentNode.children;
+  } else {
+    elseLi = domEvent.parentNode.parentNode.children;
+  }
+  for (var i = 0, elseLil = elseLi.length; i < elseLil; i++) {
+    if (elseLi[i].children[1]) {
+      elseLi[i].classList.remove('shake');
+      elseLi[i].children[1].classList.add('none');
+      elseLi[i].children[1].classList.remove('block');
+    }
+  }
+}
+
+function addStyle(domEvent) {
+  showOrHidDialog('show'); // 显示遮照层
+
+  let elseLi = domEvent.parentNode.parentNode.children;
+  for (var i = 0, elseLil = elseLi.length; i < elseLil; i++) {
+    if (elseLi[i].children[1]) {
+      elseLi[i].classList.add('shake');
+      elseLi[i].children[1].classList.remove('none');
+      elseLi[i].children[1].classList.add('block');
+    }
+  }
+}
 
 function showOrHidDialog(type) {
   if (type === 'show') {
@@ -239,10 +189,29 @@ function showOrHidDialog(type) {
 }
 
 // 拖拽事件
-var dragDom = document.getElementById('use-frequently-ul');
-new Sortable(dragDom, {
+new Sortable(useFrequentlyUl, {
   swapThreshold: 1,
   animation: 150,
   direction: 'horizontal', // 拖拽方向 (默认情况下会自动判断方向)
   sort: true, // boolean 定义是否列表单元是否可以在列表容器内进行拖拽排序
 });
+// 拖拽以后，需要将新拖拽后数据赋值给useFrequentlyArr，在重新渲染
+
+// //设置配置
+// var option = {
+//   animation: 1000,
+//   // draggable: ".item",
+//   // direction: 'vertical',
+//   // forceFallback: true,
+//   // group: 'itxst.com',
+//   //拖动结束
+//   onEnd: function (evt) {
+//     console.log(evt);
+//     //获取拖动后的排序
+//     var arr = sortable2.toArray();
+//     // document.getElementById("msg1").innerHTML = "A组排序结果：" + JSON.stringify(sortable1.toArray());
+//     document.getElementById("msg2").innerHTML = "B组排序结果：" + JSON.stringify(sortable2.toArray());
+//   },
+// };
+// //初始化
+// var sortable2 = Sortable.create(useFrequentlyUl, option);
